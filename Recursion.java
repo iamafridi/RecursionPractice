438a6610aa33c7c4ac0e3f44f0739e4be7bf49a0
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-04-27 18:45:49 +0600

    Fibonnacci

--- Recursion.java
+++ Recursion.java
@@ -1,29 +1,57 @@
+// Fibonacci Series Using Recursion 
 
+public class Recursion {
 
-public class Recursion{
-
-    public static void printRecursion(int i, int n, int sum) {
-if(i==n){
-    sum+=i;
-    System.out.println(sum);
-    return;
-}
-        sum +=i;
-        printRecursion(i+1,n,sum);
-        
+    public static void Fibo(int a, int b, int n) {
+        if (n == 0) {
+            return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        Fibo(b, c, n - 1);
     }
+
     public static void main(String[] args) {
-        printRecursion(1, 5, 0);
+        int a = 0, b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7;
+        Fibo(a, b, n - 2);
+
     }
 }
 
-
-
-
-
-
+// Factorial of n
+// public class Recursion{
+//     public static int calculateFact(int n) {
+//        if(n==1 || n==0){
+//         return 1;
+//        }
+//         int fact_n1 = calculateFact(n-1);
+//        int fact_n = n*fact_n1;
+//       return fact_n;
+//     }
+//     public static void main(String[] args) {
+//         int n=3;
+//      int ans =  calculateFact(n);
+//      System.out.println(ans);
+//     }
+// }
+// public class Recursion{
+//     public static void printRecursion(int i, int n, int sum) {
+// if(i==n){
+//     sum+=i;
+//     System.out.println(sum);
+//     return;
+// }
+//         sum +=i;
+//         printRecursion(i+1,n,sum);
+//     }
+//     public static void main(String[] args) {
+//         printRecursion(1, 5, 0);
+//     }
+// }
 // public class Recursion {
-
 //     public static void printRecursion(int n) {
 //         if (n == 6) {
 //             return;
@@ -31,16 +59,12 @@ if(i==n){
 //         System.out.println(n);
 //         printRecursion(n+1);
 //     }
-
 //     public static void main(String[] args) {
 //         int n = 1;
 //         printRecursion(n);
 //     }
 // }
-
-
 // public class Recursion {
-
 //     public static void printRecursion(int n) {
 //         if (n == 0) {
 //             return;
@@ -48,7 +72,6 @@ if(i==n){
 //         System.out.println(n);
 //         printRecursion(n-1);
 //     }
-
 //     public static void main(String[] args) {
 //         int n = 5;
 //         printRecursion(n);
