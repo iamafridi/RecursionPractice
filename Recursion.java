702e0ecdc741aa6e4faf0a81f0b2b7287eb2ac0b
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-04-27 19:02:25 +0600

    X to the power n

--- Recursion.java
+++ Recursion.java
@@ -1,26 +1,47 @@
-// Fibonacci Series Using Recursion 
 
+// Working on x^n 
 public class Recursion {
 
-    public static void Fibo(int a, int b, int n) {
-        if (n == 0) {
-            return;
-        }
-        int c = a + b;
-        System.out.println(c);
-        Fibo(b, c, n - 1);
+    public static int calculateX(int x, int n) {
+if(n==0){  //base case 1
+    return 1;
+}
+if(x==1){ //base case 2
+    return 1;
+}
+
+
+int XPowerNminusOne = calculateX(x, n-1); //kaaj
+int xPower = x * XPowerNminusOne;
+return xPower;
     }
 
     public static void main(String[] args) {
-        int a = 0, b = 1;
-        System.out.println(a);
-        System.out.println(b);
-        int n = 7;
-        Fibo(a, b, n - 2);
-
+        int x = 2;
+        int n = 5;
+        int ans = calculateX(x, n);
+        System.out.println(ans);
     }
 }
 
+// Fibonacci Series Using Recursion 
+// public class Recursion {
+//     public static void Fibo(int a, int b, int n) {
+//         if (n == 0) {
+//             return;
+//         }
+//         int c = a + b;
+//         System.out.println(c);
+//         Fibo(b, c, n - 1);
+//     }
+//     public static void main(String[] args) {
+//         int a = 0, b = 1;
+//         System.out.println(a);
+//         System.out.println(b);
+//         int n = 7;
+//         Fibo(a, b, n - 2);
+//     }
+// }
 // Factorial of n
 // public class Recursion{
 //     public static int calculateFact(int n) {
