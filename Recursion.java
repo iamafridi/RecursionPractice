347e2062804b4fbb59fc452081fed16cf18b934e
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-04-27 12:11:08 +0600

    Initial Stage

---
+++ Recursion.java
@@ -0,0 +1,16 @@
+
+public class Recursion {
+
+    public static void printRecursion(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printRecursion(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        printRecursion(n);
+    }
+}
